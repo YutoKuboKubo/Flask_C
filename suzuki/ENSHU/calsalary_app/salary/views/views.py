92df708cc1f7e888df34813aff5690bd7b2b3be1
SuzukiRika00 <suzuki.rika.zz@mynavi.jp>
2023-06-02 04:08:45 +0000

    バリデーション追加

--- suzuki/ENSHU/calsalary_app/salary/views/views.py
+++ suzuki/ENSHU/calsalary_app/salary/views/views.py
@@ -1,6 +1,7 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from salary import app
 from decimal import Decimal, ROUND_HALF_UP
+import re
 
 @app.route('/', methods=['GET', 'POST'])
 def input():
@@ -10,19 +11,43 @@ def input():
 @app.route('/output', methods=['GET', 'POST'])
 def output():
     session["input_data"] = ""
+    if request.form["salary"] == "":
+        flash('ERROR！ 給与が未記入です。')
+        return redirect(url_for('input')) # アラート
+    if int(request.form["salary"]) > 9999999999:
+        flash('ERROR！ 給与には最大9,999,999,999まで入力可能です。')
+        return redirect(url_for('input'))
+    if int(request.form["salary"]) < 0:
+        flash('ERROR！ 給与にはマイナスの値は入力できません。')
+        return redirect(url_for('input'))
 
-    #計算
-    if request.method == "POST":
+    if request.method == "POST": # 給与
         input_salary = int(request.form["salary"])
         if (input_salary > 1000000):
             tax = (input_salary - 1000000) * 0.2 + 100000
         else:
             tax = input_salary * 0.1
         tax = Decimal(str(tax)).quantize(Decimal("0"), rounding=ROUND_HALF_UP)
-        #支給額 = 給与 - 税額
         pay = input_salary - tax
     return render_template("output.html",  salary=input_salary, tax=tax, pay=pay)
+
+
 
 #   カンマ区切り出力の為に出力テキストをView側で作成する場合       
 #        text = "給与:" + "{:,}".format(input_salary) + " 円の場合、支給額：" + "{:,}".format(pay) + " 円、税額：" + "{:,}".format(tax) + " 円"
 #    return render_template("output.html", text=text)
+
+    #バリデーション    
+    if request.form["salary"] == "":
+        flash('給与が未入力です。入力してください。')
+        return redirect(url_for("input"))
+
+    if int(request.form["salary"]) > 9999999999:
+        flash('給与には最大9,999,999,999まで入力可能です。')
+        session["input_data"] = request.form["salary"]
+        return redirect(url_for("input"))
+
+    if int(request.form["salary"]) < 0:
+        flash('給与にはマイナスの値は入力できません。')
+        session["input_data"] = request.form["salary"]
+        return redirect(url_for("input")) 
