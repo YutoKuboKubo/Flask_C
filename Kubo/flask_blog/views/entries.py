b433152b59c6a932ceef56a75aaa64b15cf4a13d
YutoKuboKubo <kubo.yuto.ik@mynavi.jp>
2023-06-02 04:35:18 +0000

    add del entry

--- Kubo/flask_blog/views/entries.py
+++ Kubo/flask_blog/views/entries.py
@@ -8,7 +8,8 @@ from flask_blog.models.entries import Entry
 def show_entries():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
-    return render_template('entries/index.html')
+    entries = Entry.query.order_by(Entry.id.desc()).all()
+    return render_template('entries/index.html', entries=entries)
 
 
 @app.route('/entries/new', methods=['GET'])
@@ -29,4 +30,44 @@ def add_entry():
     db.session.add(entry)
     db.session.commit()
     flash('新しく記事が作成されました')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/entries/<int:id>', methods=['GET'])
+def show_entry(id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry.query.get(id)
+    return render_template('entries/show.html', entry=entry)
+
+
+@app.route('/entries/<int:id>/edit', methods=['GET'])
+def edit_entry(id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry.query.get(id)
+    return render_template('entries/edit.html', entry=entry)
+
+
+@app.route('/entries/<int:id>/update', methods=['POST'])
+def update_entry(id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry.query.get(id)
+    entry.title = request.form['title']
+    entry.text = request.form['text']
+    db.session.merge(entry)
+    db.session.commit()
+    flash('記事が更新されました')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/entries/<int:id>/delete', methods=['POST'])
+def delete_entry(id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry.query.get(id)
+    db.session.delete(entry)
+    db.session.commit()
+    flash('投稿が削除されました')
     return redirect(url_for('show_entries'))
