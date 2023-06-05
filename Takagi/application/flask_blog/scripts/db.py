9432dce2d7dc5fc01395897276cf0ecec4241e4b
NoeTakagi <takagi.noe.tm@mynavi.jp>
2023-06-05 07:59:39 +0000

    add css

--- Takagi/application/flask_blog/scripts/db.py
+++ Takagi/application/flask_blog/scripts/db.py
@@ -1,7 +1,6 @@
 from flask_script import Command
 from flask_blog import db
 from flask_blog.models.entries import Entry
-from flask_blog.login.create_login import Login
 
 class InitDB(Command):
     "create database"
--- Takagi/application/flask_blog/views/views.py
+++ Takagi/application/flask_blog/views/views.py
@@ -1,6 +1,14 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
-from flask_blog import db
+from functools import wraps
+
+def login_required(view):
+    @wraps(view)
+    def inner(*args, **kwargs):
+        if not session.get('logged_in'):
+            return redirect(url_for('logged_in'))
+        return view(*args, **kwargs)
+    return inner
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
