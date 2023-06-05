80adb920f1915a50ff3f03149c9371f92d8c36d8
YutoKuboKubo <kubo.yuto.ik@mynavi.jp>
2023-06-05 00:53:28 +0000

    add new holiday function

--- Kubo/holiday_app/holiday/views/input.py
+++ Kubo/holiday_app/holiday/views/input.py
@@ -3,7 +3,7 @@ from holiday import app
 from holiday import db
 from holiday.models.mst_holiday import Holiday
 
-
+# input.htmlの処理
 @app.route('/', methods=["GET", "POST"])
 def new_holiday():
     if request.method == "POST":
@@ -13,10 +13,17 @@ def new_holiday():
         )
         db.session.add(holiday)
         db.session.commit()
+        # 入力された値を保存
+        session['holi_date'] = request.form['holi_date']
+        session['holi_text'] = request.form['holi_text']
         return redirect(url_for('result'))
     return render_template('input.html')
 
-
+# result.htmlの処理（入力された値を表示する）
 @app.route('/maintenance_date', methods=["GET"])
 def result():
-    return render_template('result.html')
+    # 入力された値を呼び出す
+    holi_date = session.get('holi_date', None)
+    holi_text = session.get('holi_text', None)
+    # 値を渡す
+    return render_template('result.html', holi_date=holi_date, holi_text=holi_text)
