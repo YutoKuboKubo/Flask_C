92fbe250e4aa07898068b004498c4d98fd8c3338
YutoKuboKubo <kubo.yuto.ik@mynavi.jp>
2023-06-05 02:31:13 +0000

    add delete function

--- Kubo/holiday_app/holiday/views/input.py
+++ Kubo/holiday_app/holiday/views/input.py
@@ -5,29 +5,48 @@ from holiday.models.mst_holiday import Holiday
 
 # input.htmlの処理
 @app.route('/', methods=["GET", "POST"])
-def new_holiday():
+def input():
     if request.method == "POST":
         # レコードが存在しているか確認(データが無ければNone)
         exists = Holiday.query.get(request.form['holi_date'])
-        # あったら更新処理
-        if exists:
-            exists.holi_text = request.form['holi_text']
-            db.session.merge(exists)
-            db.session.commit()
-            # 新規登録か判断する用のセッション
-            session['status'] = 'update'
-        else:
-            # 新しいレコードを追加
-            holiday = Holiday(
-                holi_date=request.form['holi_date'],
-                holi_text=request.form['holi_text']
-            )
-            db.session.add(holiday)
-            db.session.commit()
-            # 新規登録か判断する用のセッション
-            session['status'] = 'add'
-        # 入力された値を保存
-        session['holi_date'] = request.form['holi_date']
-        session['holi_text'] = request.form['holi_text']
+        # 押されたボタンの判定用変数
+        value = request.form['button']
+        # 新規登録・更新ボタンが押された場合
+        if value == 'add':
+            # あったら更新処理
+            if exists:
+                exists.holi_text = request.form['holi_text']
+                db.session.merge(exists)
+                db.session.commit()
+                # 新規登録か判断する用のセッション
+                session['status'] = 'update'
+                # 入力された値を保存
+                session['holi_date'] = request.form['holi_date']
+                session['holi_text'] = request.form['holi_text']
+            else:
+                # 新しいレコードを追加
+                holiday = Holiday(
+                    holi_date=request.form['holi_date'],
+                    holi_text=request.form['holi_text']
+                )
+                db.session.add(holiday)
+                db.session.commit()
+                # 新規登録か判断する用のセッション
+                session['status'] = 'add'
+                # 入力された値を保存
+                session['holi_date'] = request.form['holi_date']
+                session['holi_text'] = request.form['holi_text']
+        # 削除ボタンが押された場合
+        elif value == 'delete':
+            if exists:
+                db.session.delete(exists)
+                db.session.commit()
+                # 新規登録か判断する用のセッション
+                session['status'] = 'delete'
+                session['holi_date'] = request.form['holi_date']
+                session['holi_text'] = exists.holi_text
+            else:
+                flash(f"{request.form['holi_date']}は、祝日マスタに登録されていません")
+                return redirect(url_for('input'))
         return redirect(url_for('result'))
     return render_template('input.html')
