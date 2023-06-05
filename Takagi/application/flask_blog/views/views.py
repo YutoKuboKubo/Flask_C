3f982c73aedbc137b9fea2c9b47b0057e789062d
NoeTakagi <takagi.noe.tm@mynavi.jp>
2023-06-05 02:22:16 +0000

    fix flask_blog

--- Takagi/application/flask_blog/views/views.py
+++ Takagi/application/flask_blog/views/views.py
@@ -1,11 +1,6 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
-
-@app.route('/')
-def show_entries():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    return render_template('entries/index.html')
+from flask_blog import db
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -25,3 +20,5 @@ def logout():
     session.pop('logged_in', None)
     flash('ログアウトしました')
     return redirect(url_for('show_entries'))
+
+    
