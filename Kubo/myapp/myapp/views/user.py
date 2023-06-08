70505f09c908b587c457ff2f93495cadcd52d48b
YutoKuboKubo <kubo.yuto.ik@mynavi.jp>
2023-06-08 01:02:09 +0000

    add user info form

--- Kubo/myapp/myapp/views/user.py
+++ Kubo/myapp/myapp/views/user.py
@@ -21,3 +21,20 @@ def user(id):
         db.session.commit()
         return redirect(url_for('home'))
     return render_template('user.html', id=id, username=user.username, password=user.password)
+
+
+@app.route('/user_list')
+@login_required
+def user_list():
+    '''ユーザの一覧を表示する用'''
+    users = User.query.all()
+    return render_template('user_list.html', users=users)
+
+
+@app.route('/user_info/<int:id>')
+@login_required
+def user_info(id):
+    '''ユーザの情報を閲覧する用'''
+    user = User.select_user_by_id(id)
+    tweets = Tweets.query.filter_by(from_user_id=user.id).all()
+    return render_template('user_info.html', user=user, tweets=tweets)
