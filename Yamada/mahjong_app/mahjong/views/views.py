ab3610ceeea404e78a8af15d8e2e313ae2819018
ShomaYamada11 <yamada.shoma.db@mynavi.jp>
2023-06-08 04:40:10 +0000

    打牌選択＋ツモ牌まで

--- Yamada/mahjong_app/mahjong/views/views.py
+++ Yamada/mahjong_app/mahjong/views/views.py
@@ -4,6 +4,9 @@ import random
 from mahjong import db
 from mahjong.models.tiles import Tiles
 
+tiles =[]
+tilelink_list = []
+
 def distribute(tile_list):
     for i in range(14):
         tile_list[i] = random.randint(1,34)
@@ -11,6 +14,8 @@ def distribute(tile_list):
         if tile_list.count(t) >= 5:
             distribute(tile_list)
         else:
+            global tiles
+            tiles = tile_list
             tile_list.sort()
 
 @app.route('/', methods=['GET', 'POST'])
@@ -22,7 +27,23 @@ def playtile():
         for j in range(14):
             tile = Tiles.query.get(tile_list[j])
             tilelinks[j] = tile.link
-        return render_template('index.html', tilelinks=tilelinks)
+        global tilelink_list
+        tilelink_list = tilelinks
+        return render_template('index.html', tilelinks=tilelink_list)
+
+@app.route('/<int:num>', methods=['GET'])
+def tileselect(num):
+    global tilelink_list
+    global tiles
+    if request.method == 'GET':
+        temp = tilelink_list[13]
+        tilelink_list[13] = tilelink_list[num]
+        tilelink_list[num] = temp
+        tumonum = random.randint(1,34)
+        tumopai = Tiles.query.get(tumonum)
+        tilelink_list[13] = tumopai.link
+        return render_template('index.html', tilelinks=tilelink_list)
+        
 
 @app.route('/tumo')
 def tumo():
